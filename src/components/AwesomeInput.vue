f71b067535c5efa0857439b2ec1028b64a7c0d5e
Роман Юрьевич Троицкий <vzhyx@ya.ru>
2023-08-05 07:36:31 +0300

    feat: v1

---
+++ src/components/AwesomeInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <label class="input__label">
+    <ValidationProvider v-slot="v">
+      <input
+        :type="type"
+        :disabled="isDisabled"
+        :name="name"
+      >
+      <span>{{ v.errors[0] }}</span>
+    </ValidationProvider>
+  </label>
+</template>
+
+<script setup lang="ts">
+
+</script>
