5ccad89d767d018a5c81e4f23a20f6a9f0ade8be
Роман Юрьевич Троицкий <vzhyx@ya.ru>
2023-08-06 20:35:09 +0300

    feat: v1

---
+++ src/components/ChamomileInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <label class="input__label">
+    <input
+      :type="type"
+      :disabled="isDisabled"
+      :name="name"
+    >
+  </label>
+</template>
+
+<script setup lang="ts">
+import { IInput } from "@/types/inputs";
+
+defineProps<IInput>()
+</script>
