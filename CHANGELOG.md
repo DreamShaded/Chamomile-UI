8169f47951061fcb554395d61cfedc9384fe15c1
Роман Юрьевич Троицкий <vzhyx@ya.ru>
2023-08-11 08:26:17 +0300

    feat: v2

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# 2.0.0 (2023-08-11)
+
+
+### Features
+
+* v1 ([5ccad89](https://gl.vzhyx.digital/open-projects/msk-vuejs/library/commits/5ccad89d767d018a5c81e4f23a20f6a9f0ade8be))
+* v2 ([8c01f9d](https://gl.vzhyx.digital/open-projects/msk-vuejs/library/commits/8c01f9dde98c4cfa28269809e8a67f547ea10105))
+* перенос в сабмодуль ([94f87e6](https://gl.vzhyx.digital/open-projects/msk-vuejs/library/commits/94f87e69c085dac283f3f91bd6c06bccc86eb9ac))
+
+
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'node:path'
-// import glob from 'fast-glob'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    dts({
+        include: [
+          'src/index.ts',
+          'src/types',
+          'src/components'
+        ],
+        exclude: ['node_modules'],
+        outDir: 'dist',
+        compilerOptions: {
+          sourceMap: true
+        },
+        copyDtsFiles: true
+      })
   ],
   esbuild: {
     drop: ['debugger'],
